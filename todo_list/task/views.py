af136516ac0f68a6f00f4dae4b45466d16b747bc
Sedigheh <sd.farasat@gmail.com>
2022-03-31 22:10:01 +0430

    Created templates for home page and task detail page

--- todo_list/task/views.py
+++ todo_list/task/views.py
@@ -6,9 +6,11 @@ class TaskListView(ListView):
     model = Task
     template_name = 'task/home.html'
     context_object_name = 'tasks'
+    context = {'title': 'Home'}
 
 
 class TaskDetailView(DetailView):
     model = Task
     template_name = 'task/details.html'
     context_object_name = 'task'
+
