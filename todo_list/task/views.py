4c6a649921587472091d926a08bcf861bc30d018
Sedigheh <sd.farasat@gmail.com>
2022-03-31 20:08:22 +0430

    Created view and url routing for the home page

--- todo_list/task/views.py
+++ todo_list/task/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Task
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task/home.html'
+    context_object_name = 'tasks'
