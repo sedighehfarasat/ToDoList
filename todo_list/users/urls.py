05d19e558648a4e0747b8fbb373e96e408d6eaa6
Sedigheh <sd.farasat@gmail.com>
2022-04-01 16:06:37 +0430

    Created registration page

--- todo_list/users/urls.py
+++ todo_list/users/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='users-login'),
     path('logout/', LogoutView.as_view(next_page='users-login'), name='users-logout'),
+    path('register/', views.UserRegisterView.as_view(), name='users-register'),
 ]
--- todo_list/users/views.py
+++ todo_list/users/views.py
@@ -1,4 +1,7 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
+from django.views.generic import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 
 
@@ -10,3 +13,15 @@ class UserLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('task-home')
 
+
+class UserRegisterView(FormView):
+    template_name = 'users/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('task-home')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(UserRegisterView, self).form_valid(form)
